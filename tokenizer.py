c0c1a9958de4541a4aab60fbabeac71adeb8e0e9
Andrew <aadoranti@gmail.com>
2022-11-23 16:53:24 -0500

    Updated paths in tokenizer to reflect repo structure

---
+++ tokenizer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from deepchem.feat.smiles_tokenizer import SmilesTokenizer
+
+data = pd.read_csv("data\input_smiles.csv")
+
+vocab = r'data\vocab.txt'
+
+smiles = list(data['SMILES'])
+
+tokenizer = SmilesTokenizer(vocab)
+print(tokenizer.get_vocab())
+
+for smile in smiles:
+    print(tokenizer.encode(smile))
