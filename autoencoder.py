a97c86330ce760ab8a3c877b23e58d8f94ee4425
aadoranta <92287049+aadoranta@users.noreply.github.com>
2022-12-13 12:14:42 -0500

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-from tokenized_smiles import TokenizedSmiles
+from tokenized_smiles import TokenizedSmiles, FullData
 from vectorquantizer import VectorQuantizer
 from torch.utils.data import DataLoader
 from tokenizer import Tokenizer
@@ -16,58 +16,69 @@ from decoder import Decoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-data = pd.read_csv(r'data\updated_smiles.csv')
-vocab = r'data\vocab.txt'
+# data = pd.read_csv(r'data\updated_smiles.csv')
+# vocab = r'data\vocab.txt'
+#
+# tokenizer = Tokenizer(data=data, vocab=vocab)
+# tokenizer.int_encoder(pad=True, save_csv=True, path=r'data\updated_smiles_tokenized.csv')
 
-tokenizer = Tokenizer(data=data, vocab=vocab)
-tokenizer.int_encoder(pad=True, save_csv=True, path=r'data\updated_smiles_tokenized.csv')
-
-tokenized_smiles = TokenizedSmiles(path=r'data\updated_smiles_tokenized.csv')
+tokenized_smiles = FullData(path=r'data\full_data_tokenized.csv', label='Metal_q')
 
 # Should be divided by size of data
-data_variance = 1 # np.var(tokenized_smiles.data)
+data_variance = 201.6591565127223 / 512 # np.var(tokenized_smiles.data)
 
 
 class Model(nn.Module):
-    def __init__(self, num_hiddens, num_embeddings, embedding_dim, commitment_cost):
+    def __init__(self, num_hiddens1, num_hiddens2, num_hiddens3, num_hiddens4, num_embeddings, embedding_dim, commitment_cost):
         super(Model, self).__init__()
 
-        self._encoder = Encoder(1,
-                                num_hiddens,
+        self._encoder = Encoder(512,
+                                num_hiddens1,
+                                num_hiddens2
                                 )
-        self._pre_vq_conv = nn.Conv1d(in_channels=num_hiddens,
+        # self._pre_vq_conv = nn.Linear(num_hiddens2, embedding_dim)
+
+        self._pre_vq_conv = nn.Conv1d(in_channels=1,
                                       out_channels=embedding_dim,
-                                      kernel_size=1,
+                                      kernel_size=3,
                                       stride=1, padding='same')
 
         self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
                                            commitment_cost)
-        self._decoder = Decoder(embedding_dim,
-                                num_hiddens,
+        self._decoder = Decoder(embedding_dim*num_hiddens2,
+                                num_hiddens3,
+                                num_hiddens4,
                                 )
 
     def forward(self, x):
+        x = torch.unsqueeze(x, dim=1)
         z = self._encoder(x)
         z = self._pre_vq_conv(z)
         loss, quantized, perplexity, _ = self._vq_vae(z)
         x_recon = self._decoder(quantized)
 
-        return loss, x_recon, perplexity
+        return loss, x_recon, perplexity, quantized
 
 
-num_hiddens = 32
-embedding_dim = 64
-num_embeddings = 128
+num_hiddens1 = 256
+num_hiddens2 = 64
+num_hiddens3 = 64
+num_hiddens4 = 512
+embedding_dim = 32
+num_embeddings = 256
 commitment_cost = 0.25
 learning_rate = 1e-3
 num_training_updates = 15000
-batch_size = 4
-num_epochs = 10
+batch_size = 1
+num_epochs = 15
 
 smiles_loader = DataLoader(tokenized_smiles, batch_size=batch_size, shuffle=True)
 
 
-model = Model(num_hiddens=num_hiddens,
+model = Model(num_hiddens1=num_hiddens1,
+              num_hiddens2=num_hiddens2,
+              num_hiddens3=num_hiddens3,
+              num_hiddens4=num_hiddens4,
               embedding_dim=embedding_dim,
               num_embeddings=num_embeddings,
               commitment_cost=commitment_cost,
@@ -76,16 +87,27 @@ model = Model(num_hiddens=num_hiddens,
 optimizer = optim.Adam(model.parameters(), lr=learning_rate, amsgrad=False)
 
 model.train()
-train_res_recon_error = []
-train_res_perplexity = []
 
-for _ in range(num_epochs):
+for epoch in range(num_epochs):
+
+    train_res_recon_error = []
+    train_res_perplexity = []
+
+    tokens = list()
+    labels = list()
+    quantizeds = list()
+
     for i, data in enumerate(smiles_loader):
 
+        data, y = data
+        data = torch.tensor(data, dtype=torch.float32)
         data = data.to(device)
         optimizer.zero_grad()
+        model.zero_grad()
 
-        vq_loss, data_recon, perplexity = model(data)
+        vq_loss, data_recon, perplexity, quantized = model(data)
+        # data_recon = torch.squeeze(data_recon)
+        # data = torch.squeeze(data)
         # Should we weight the reconstruction loss higher?
         recon_error = F.mse_loss(data_recon, data) / data_variance
         loss = recon_error + vq_loss
@@ -95,6 +117,12 @@ for _ in range(num_epochs):
 
         train_res_recon_error.append(recon_error.item())
         train_res_perplexity.append(perplexity.item())
+        data_sav = torch.squeeze(data)
+        label_sav = torch.squeeze(y)
+        q_sav = torch.flatten(quantized)
+        tokens.append(list(data_sav.cpu().detach().numpy()))
+        labels.append(list(y.cpu().detach().numpy()))
+        quantizeds.append(list(q_sav.cpu().detach().numpy()))
 
         if (i + 1) % 1000 == 0:
             print('%d iterations' % (i + 1))
@@ -102,5 +130,13 @@ for _ in range(num_epochs):
             print('perplexity: %.3f' % np.mean(train_res_perplexity[-1000:]))
             print()
 
+    print("--------DONE EPOCH {}--------".format(epoch + 1))
+    print(np.mean(train_res_recon_error))
     print(list(data))
     print(list(data_recon))
+    torch.save(model, 'autoencoder_model-{}.pt'.format(epoch + 1))
+    df = pd.DataFrame()
+    df['tokens'] = tokens
+    df['embeddings'] = quantizeds
+    df['label'] = labels
+    df.to_csv(r'data\embeddings\encoded_{}.csv'.format(epoch))
